a9b34b4f556766a97c4a22f7f326bb6dd69be3be
fluffcoding <m.mashood@khi.iba.edu.pk>
2020-07-10 18:22:10 +0500

    Added Profile and Campaign models

--- src/meme_marketing/settings.py
+++ src/meme_marketing/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+
+    # Own
+    'users',
+    'business',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Specifying custom user model
+AUTH_USER_MODEL = 'users.User'
